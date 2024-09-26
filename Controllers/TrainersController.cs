976d66e09b2b31b0ef3d768a561fc10e9af200d1
victor <6280636@cegepmontpetit.ca>
2024-09-25 21:54:55 -0400

    FCT Modification dans Trainer controller
    Modification de l'action détail pour utiliser le service

--- Controllers/TrainersController.cs
+++ Controllers/TrainersController.cs
@@ -40,10 +40,10 @@ namespace JuliePro.Controllers
             {
                 return NotFound();
             }
-
-            var trainer = await _context.Trainer
-                .Include(t => t.Speciality)
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var trainer = await _serviceT.GetByIdAsync((int)id);
+            //var trainer = await _context.Trainer
+            //    .Include(t => t.Speciality)
+            //    .FirstOrDefaultAsync(m => m.Id == id);
             if (trainer == null)
             {
                 return NotFound();
@@ -81,74 +81,22 @@ namespace JuliePro.Controllers
             {
                 if (trainer.Id == 0)
                 {
-                    _context.Trainer.Add(trainer);
+                    //_context.Trainer.Add(trainer);
+                    await _serviceT.CreateAsync(trainer);
                     TempData["Success"] = $"{trainer.LastName} trainer added";
                 }
                 else
                 {
-                    _context.Trainer.Update(trainer);
+                    await _serviceT.EditAsync(trainer);
                     TempData["Success"] = $"{trainer.LastName} trainer update";
                 }
 
-                await _context.SaveChangesAsync();
+                //await _context.SaveChangesAsync();
                 return this.RedirectToAction(nameof(Index));
             }
             //ViewData["SpecialityId"] = new SelectList(_context.Speciality, "Id", "Name", trainer.SpecialityId);
             return View(trainer);
-        }
-
-        // GET: Trainers/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var trainer = await _context.Trainer.FindAsync(id);
-        //    if (trainer == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    ViewData["SpecialityId"] = new SelectList(_context.Speciality, "Id", "Name", trainer.SpecialityId);
-        //    return View(trainer);
-        //}
-
-        //// POST: Trainers/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,Photo,SpecialityId")] Trainer trainer)
-        //{
-        //    if (id != trainer.Id)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(trainer);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!TrainerExists(trainer.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    ViewData["SpecialityId"] = new SelectList(_context.Speciality, "Id", "Name", trainer.SpecialityId);
-        //    return View(trainer);
-        //}
+        }      
 
         // GET: Trainers/Delete/5
         public async Task<IActionResult> Delete(int? id)
