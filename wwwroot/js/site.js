1d0b8d4b95c22ed4cfd839c25be783971826dffb
victor <6280636@cegepmontpetit.ca>
2024-09-29 12:33:07 -0400

    FCT Modification dans JQuery
    Modifiez les paramètres JQuery de Summernote afin qu’il ait les outils suivants sur la barre d’outils : Fonts, bold, Italique, liste 1-2-3

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
 // Write your JavaScript code.
 
 $(document).ready(function () {
@@ -11,10 +12,10 @@ $(document).ready(function () {
 
     // summernote
 
-    $('#summernote').summernote({
+    $('.summernote').summernote({
         placeholder: 'Description ...',
-        tabsize: 2,
-        height: 120,
+        tabsize: 200,
+        //height: 120,
         toolbar: [
             // Limiter les outils visibles
             ['style', ['bold', 'italic']],
